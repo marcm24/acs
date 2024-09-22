30bf10bde9508ca805847e27a009e25b479099e0
Marc Melendez <marcmelendez24@gmail.com>
2024-09-22 18:40:49 -0400

    add cache read/write latency test for task 1

---
+++ project1/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cstdint>
+
+void read_latency(int mem_size, int iterations){
+    int mem_alloc = mem_size / sizeof(int); // allocation in the memory
+    std::vector<int> memory(mem_alloc); // initialize array
+
+    int tmp = 0; // int for reading
+
+    auto start = std::chrono::high_resolution_clock::now(); // start measuring time
+    for (int i = 0; i < iterations; i++) {
+        tmp += memory[i % mem_alloc]; // access within the memory
+    }
+    auto end = std::chrono::high_resolution_clock::now(); // stop measuring time
+
+    auto time = end - start; // time difference
+
+    double access_latency = time.count() / iterations;
+
+    std::cout << "read latency per access: " << access_latency <<  "ns" << std::endl;
+}
+
+void write_latency(int mem_size, int iterations){
+    int mem_alloc = mem_size / sizeof(int); // allocation in the memory
+    std::vector<int> memory(mem_alloc); // initialize array
+
+    auto start = std::chrono::high_resolution_clock::now(); // start measuring time
+    for (int i = 0; i < iterations; i++) {
+        memory[i % mem_alloc] = i; // write to memory
+    }
+    auto end = std::chrono::high_resolution_clock::now(); // stop measuring time
+
+    auto time = end - start; // time difference
+
+    double access_latency = time.count() / iterations;
+
+    std::cout << "write latency per access: " << access_latency <<  "ns" << std::endl;
+}
+
+int main(){
+    int l1_cache_size = 192 * 1024; // in kilobytes
+    int l2_cache_size = 3 * 1024 * 1024; // in megabytes
+    int l3_cache_size = 8 * 1024 * 1024; // in megabytes
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "L1 Cache (" << l1_cache_size / 1024 << "KB):" << std::endl;
+    read_latency(l1_cache_size, 1000);
+    write_latency(l1_cache_size, 1000);
+    std::cout << "------------------------------" << std::endl;
+
+
+    std::cout << "L2 Cache (" << l2_cache_size / 1024 / 1024 << "MB):"<< std::endl;
+    read_latency(l2_cache_size, 1000);
+    write_latency(l2_cache_size, 1000);
+    std::cout << "------------------------------" << std::endl;
+
+
+    std::cout << "L3 Cache (" << l3_cache_size / 1024 / 1024 << "MB):" << std::endl;
+    read_latency (l3_cache_size, 1000);
+    write_latency(l3_cache_size, 1000);
+    std::cout << "------------------------------" << std::endl;
+
+    /*
+    std::cout << "Main Memory (" << ADD_MAIN_MEMORY_HERE / 1024 / 1024 / 1024 << "GB):" << std::endl;
+    read_latency(main_size, 1000);
+    write_latency(main_size, 1000);
+    std::cout << "------------------------------" << std::endl;
+    */
+
+}
---
+++ project1/writeup.md
@@ -0,0 +1,5 @@
+Experiments being conducted on the Ryzen 5 Pro 4650U with 6 cores. The cache sizes are as follows:
+
+L1 Cache Size: 32 KB (Per Core) [Total 192 KB]
+L2 Cache Size:  512 KB (Per Core) [Total 3.072 MB]
+L3 Cache Size: 4 MB (Shared between set of 2 cores) [Total 8 MB]
